ad1e141dd27ea603c431b7efd360fee2e6df41f3
ChiamakaUI <55190142+ChiamakaUI@users.noreply.github.com>
2021-03-04 11:32:59 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './App.css';
+import { Redirect, Route,Switch } from 'react-router-dom';
 import Header from './components/Header';
 import ProductDetails from "./components/ProductDetails";
 import ProductList from "./components/ProductList";
@@ -12,8 +13,19 @@ function App() {
       <Header />
          <ProductList />
         <ProductDetails />
+      <Switch>
+        <Redirect 
+          exact 
+          from="/" 
+          to={
+            {
+              pathname:"/facemasks"
+            }
+          }
+        />
+        <Route exact path="/facemasks" component={ProductList}/>
+        <Route exact path="/facemasks/:id" component={ProductDetails}/>
+      </Switch>
     </div>
   );
 }
-
-export default App;
