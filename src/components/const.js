5af6f1c4c34b3c485e1623587eeb9ce6380e3429
ChiamakaUI <55190142+ChiamakaUI@users.noreply.github.com>
2021-03-04 12:25:07 +0100

    Create const.js

---
+++ src/components/const.js
@@ -0,0 +1,46 @@
+import Uneo from "./Images/Uneo.PNG";
+import Unos from "./Images/Unos.PNG";
+import Usp from "./Images/Usp.PNG";
+import Hero from "./Images/Hero.PNG";
+import Wild from "./Images/Wild.PNG";
+
+const masks = [
+    {
+      id: 1,
+      image: Unos,
+      name: "Uneo",
+      description: "Unisex Pack of 2",
+      price: 199
+    },
+    {
+      id: 2,
+      image: Usp,
+      name: "US Polo",
+      description: "Unisex Pack of 3",
+      price: 299
+    },
+    {
+      id: 3,
+      image: Hero,
+      name: "Hero",
+      description: "Unisex Pack of 2",
+      price: 199
+    },
+    {
+      id: 4,
+      image: Uneo,
+      name: "Uneo",
+      description: "Unisex Pack of 2",
+      price: 199
+    },
+    {
+      id: 5,
+      image: Wild,
+      name: "Wild Craft",
+      description: "Unisex Pack of 3",
+      price: 150
+    },
+  
+];
+
+export { masks };
