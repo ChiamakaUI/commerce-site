98dc85daecd597f4a4be1f2b681b682c07eeb5d4
ChiamakaUI <55190142+ChiamakaUI@users.noreply.github.com>
2021-03-04 11:37:38 +0100

    Update ProductDetails.js

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,32 +1,50 @@
-import React from 'react'
+import React from 'react';
+import { masks } from './const';
 import { Container } from 'react-bootstrap';
 import Carousel from "react-bootstrap/Carousel";
-import Unos from "./Images/Unos.PNG";
+// import Unos from "./Images/Unos.PNG";
 
+const ProductDetails = (props) => {
 
+    const [state, setState] = React.useState({
+      loading: true,
+      mask: undefined
+    });
 
+    React.useEffect(()=>{
+      // eslint-disable-next-line eqeqeq
+      let mask = masks.find((item)=>item.id == props.match.params.id);
+      if(typeof mask !== 'undefined'){
+        setState(state=>({loading: false, mask: mask}))
+      }
+    }, [setState, props]);
 
-
-const ProductDetails = () => {
     return (
-      <Container fluid>
-        <Carousel>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={Unos}
-              alt="First slide"
-              style={{ width: "100px" }}
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img className="d-block  w-100" src={Unos} alt="Second slide" />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img className="d-block  w-100" src={Unos} alt="Third slide" />
-          </Carousel.Item>
-        </Carousel>
-      </Container>
+      <>
+        {
+          state.loading ? <div>Loading...</div> :
+            state.mask === undefined ? <div>Not Found</div>
+              : <Container fluid>
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={state.mask.image}
+                  alt="First slide"
+                  style={{ width: "100px" }}
+                />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img className="d-block  w-100" src={state.mask.image} alt="Second slide" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img className="d-block  w-100" src={state.mask.image} alt="Third slide" />
+              </Carousel.Item>
+            </Carousel>
+          </Container>
+        }
+
+      </>
     );
 }
 
